d2a48337457a0a5c577d3eb8cae34dc3302daf24
Natali <NataliBezguskova@yandex.ru>
2022-10-19 16:15:59 +0300

    done with info section

--- src/components/info/Info.js
+++ src/components/info/Info.js
@@ -7,18 +7,18 @@ export default function Info () {
   const currentLang = useMemo(() => locales[language], [language])
   return (
       <div className="info">
-        {currentLang.info.about}
-        Функционал:
+        <div>{currentLang.info.about}</div>
+        <div>{currentLang.info.functionality}</div>
         <ul>
-          <li>Создание карточки задач с заголовком.</li>
-          <li>Добавление задачи с заголовком и описанием (опционально)</li>
-          <li>Просмотр задачи (полное описание, заголовок) и редактирование в модальном окне</li>
-          <li>Возможность удаления карточки задач вместе с задачами/ удаление конкретной задачи</li>
-          <li>Ручное перемещение задач внутри группы и между группами, ручное изменение порядка карточек с задачами с помощью перемещения</li>
-          <li>Возможность полной очистки приложения</li>
-          <li>Локальное состояние сохраняется в браузере</li>
-          <li>Адаптация интерфейса для десктопов, таблетов и мобильных устройств.</li>
-        </ul>
+          <li>{currentLang.info.topic_1}</li>
+          <li>{currentLang.info.topic_2}</li>
+          <li>{currentLang.info.topic_3}</li>
+          <li>{currentLang.info.topic_4}</li>
+          <li>{currentLang.info.topic_5}</li>
+          <li>{currentLang.info.topic_6}</li>
+          <li>{currentLang.info.topic_7}</li>
+          <li>{currentLang.info.topic_8}</li>
+       </ul>
       </div>
   )
 }
--- src/store/locales/index.js
+++ src/store/locales/index.js
@@ -1,14 +1,30 @@
 export const locales = {
   EN: {
     info: {
-      about: "ToDo Apр - is a simple version of the Trello writing using React. Used only two libraries:  Без использования дополнительных библиотек: использованы только (redux, RTK, uniqid)",
-
+      about: "ToDo Apр - is a simple version of the Trello writing using React. There are only tree additional libraries: redux, RTK, uniqid.",
+      functionality: "Functionality:",
+      topic_1: "Create card with title for tasks.",
+      topic_2: "Add a task with title and description (optional) to card.",
+      topic_3: "Open task details in modal and ability to edit it's title and description.",
+      topic_4: "Delete card with all its tasks/delete task from card.",
+      topic_5: "Drag and drop tasks to reorder in card or to move it in another card.",
+      topic_6: "Reset app.",
+      topic_7: "Saving local app state.",
+      topic_8: "Responsive design (desktop, tablet, mobile).",
     }
   },
   RU: {
     info: {
-      about: "Приложение ToDo Apр - это упрощенная версия Trello написанный на React. Без использования дополнительных библиотек: использованы только (redux, RTK, uniqid)",
-
+      about: "Приложение ToDo Apр - это упрощенная версия Trello написанный на React. Из дополнительных библиотек использованы лишь: redux, RTK, uniqid.",
+      functionality: "Функционал:",
+      topic_1: "Создание карточки задач с заголовком.",
+      topic_2: "Добавление задачи с заголовком и описанием (опционально)\n",
+      topic_3: "Просмотр задачи (полное описание, заголовок) и редактирование в модальном окне.",
+      topic_4: "Возможность удаления карточки задач вместе с задачами/ удаление конкретной задачи.",
+      topic_5: "Ручное перемещение задач внутри группы и между группами, ручное изменение порядка карточек с задачами с помощью перемещения.",
+      topic_6: "Возможность полной очистки приложения.",
+      topic_7: "Локальное состояние сохраняется в браузере",
+      topic_8: "Адаптация интерфейса для десктопов, таблетов и мобильных устройств.",
     }
   },
 }
